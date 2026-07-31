dc183a0c0f0bb88b3266ac937fb10c77f32f1528
Udhay Chaudhary <udhay2007ch@gmail.com>
2026-07-31 15:08:33 +0530

    Time: 103 ms (25.98%), Space: 124.9 MB (9.64%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int>freq;
+        for(int i=0;i<nums.size();i++){
+            if(freq.find(nums[i])!=freq.end()){
+                if(i-freq[nums[i]]<=k){
+                    return true;
+                }
+            }
+            freq[nums[i]]=i;
+
+        }
+        return false;
+    }
+};
